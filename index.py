1077db95db9bac4ca6c84d1c3558b400a7db440e
Mikkel Eltervåg <mikkeleltervaag@gmail.com>
2016-03-09 20:12:20 +0100

    Small changes

--- index.py
+++ index.py
@@ -31,11 +31,12 @@ while True:
 
 	# Draw Graph
 	#indoorTemperature.drawGraph(10,70,1660,970, size=3, color=green, hSeperator="hour", vSeperator=1, smooth=5)
-	indoorTemperature.drawGraph(10,70,1660,400, size=3, color=green, hSeperator="hour", vSeperator=1, smooth=5)
-	indoorHumidity.drawGraph(10,480,1660,400, size=3, color=blue, hSeperator="hour", vSeperator=1, smooth=5)
+	indoorTemperature.drawGraph(10,70,1660,480, size=3, color=green, hSeperator="hour", vSeperator=1, smooth=5)
+	indoorHumidity.drawGraph(10,560,1660,480, size=3, color=blue, hSeperator="hour", vSeperator=1, smooth=5)
 
 	# Top of screen
 	screen.blit(topText.render(str(indoorTemperature.getLastData())+unichr(176).encode("latin-1")+"C", True, white), (10, 0))
+	screen.blit(topText.render(str(indoorHumidity.getLastData())+"%", True, white), (300, 0))
 	screen.blit(topText.render(strftime("%H:%M", time.localtime()), True, white), (screenWidth-185, 0))
 	
 	#Update screen
